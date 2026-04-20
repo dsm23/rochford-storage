6d473d8a536cddc7f0319a96df60398e505a6a60
David Murdoch <dsmurdoch357@gmail.com>
2026-04-20 22:47:31 +0100

    fix(marker): force leaflet marker into bundle

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useRef } from "react";
-import L, { LatLng } from "leaflet";
+import { Icon, LatLng, map as lmap, marker, tileLayer } from "leaflet";
+import type { Map as LMap, MapOptions } from "leaflet";
 import FullScreen from "leaflet.fullscreen";
+import markerRetina from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 import "leaflet/dist/leaflet.css";
 import "leaflet.fullscreen/dist/Control.FullScreen.css";
 
-const config: L.MapOptions = {
+const config: MapOptions = {
   minZoom: 7,
   maxZoom: 18,
 };
@@ -16,30 +20,42 @@ const fullScreenControl = new FullScreen({
 
 const latlng = new LatLng(51.57784, 0.71992);
 
+const customIcon = new Icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerRetina,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // Standard Leaflet size
+  iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // Point from which the popup should open relative to the iconAnchor
+  shadowSize: [41, 41], // Size of the shadow
+});
+
 const noop = () => {};
 
 const Map = () => {
   const refMap = useRef<HTMLDivElement>(null);
-  const mapInstance = useRef<L.Map>(null);
+  const mapInstance = useRef<LMap>(null);
 
   useEffect(() => {
     // Prevent double-initialization in React 18 Strict Mode
     if (mapInstance.current) return noop;
 
     if (refMap.current) {
-      const map = L.map(
-        refMap.current satisfies HTMLDivElement,
-        config,
-      ).setView(latlng, 16);
+      const map = lmap(refMap.current satisfies HTMLDivElement, config).setView(
+        latlng,
+        16,
+      );
 
       map.addControl(fullScreenControl);
 
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(latlng).addTo(map).bindPopup("Rochford Storage");
+      marker(latlng, { icon: customIcon })
+        .addTo(map)
+        .bindPopup("Rochford Storage");
 
       mapInstance.current = map;
     }
